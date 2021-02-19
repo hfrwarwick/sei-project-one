25d0c474f3e3bc8c8c0ac4610ecdca74c7db5439
Harry <harryfrwarwick@gmail.com>
2021-02-19 13:29:27 +0000

    Added css for grid

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,14 @@
 function init() {
-console.log("CONTENT LOADED")
+// * Variables
+  const width = 10
+  const cellCount = width * width
+  const cells = []
+  const mandoClass = 'mando'
 
+  // * Add Mando to grid
+  function addMando(position) { 
+    cells[position].classList.add(mandoClass) 
+}
 
 
 

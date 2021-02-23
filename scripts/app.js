acecdba65c6c9358e5752942601005832b9dda16
Harry <harryfrwarwick@gmail.com>
2021-02-23 12:52:10 +0000

    Troopers move, but dont bounce off edge

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,26 @@
 function init() {
 
   // * Variables
+
   const grid = document.querySelector('.grid')
-  
   const width = 15
   const cellCount = width * width
   const cells = []
-
-  // const scoreDisplay = document.querySelector('#score-display')
-  // const livesDisplay = document.querySelector('#lives-display')
 
   const mandoClass = 'mando'
   const trooperClass = 'troopers'
   const mandoStartPosition = 202
   let mandoCurrentPosition = 202
   let troopersCurrentPosition = 0
-  // let trooperDestroyed = []
+  // let trooperDestroyed = [] 
+  let trooperID
+  
+  // const scoreDisplay = document.querySelector('#score-display')
+  // const livesDisplay = document.querySelector('#lives-display')
+  
 
   // * Make a grid
+
   function createGrid() {
     for (let i = 0; i < cellCount; i++) {
       const cell = document.createElement('div')
@@ -30,28 +33,28 @@ function init() {
   }
 
   // * Add Mando to grid
+
   function addMando(position) { 
     cells[position].classList.add(mandoClass) 
   }
 
   // * Remove Mando from the grid
+
   function removeMando(position) {
     cells[position].classList.remove(mandoClass)
   }
 
   // * Add Troopers 
-  
+
+  const stormTroopers = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 
+    15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+    30, 31, 32, 33, 34, 35, 36, 37, 38, 39
+  ]
   function addTroopers(troopersCurrentPosition) { 
-    const stormTroopers = [
-      0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 
-      15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
-      30, 31, 32, 33, 34, 35, 36, 37, 38, 39
-    ]
+    
     stormTroopers.forEach(troopers => cells[troopersCurrentPosition + troopers].classList.add('troopers'))
   }
-
-
-
 
   // * Move Mando left to right
 
@@ -70,6 +73,27 @@ function init() {
     
     addMando(mandoCurrentPosition)
   }
+
+  // * Move Troopers 
+  
+  let direction = 1
+
+  function moveTroopers() {
+
+    for (let i = 0; i <= stormTroopers.length - 1, i++) {
+      cells[stormTroopers[i]].classList.remove('troopers') 
+    }
+    for (let i = 0; i <= stormTroopers.length - 1, i++) {
+      stormTroopers[i] += direction
+    }
+    for (let i = 0; i <= stormTroopers.length - 1, i++) {
+      cells[stormTroopers[i]].classList.add('troopers')
+    }
+  }
+  trooperID = setInterval(moveTroopers, 500)
+
+  // * Lasers
+
 
   // * Event listeners
   document.addEventListener('keyup', handleKeyUp)
